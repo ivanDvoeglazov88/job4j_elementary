9a0d2f854cc2b6edcf615de1dd91f4bf10e2a74e
Иван <id@stk-online.ru>
2024-06-06 14:21:59 +0500

    4.1. Разбор первой программы. [#159033 #370328]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ivan Dvoeglazov");
+        System.out.println("06.06.2024");
+    }
+}
