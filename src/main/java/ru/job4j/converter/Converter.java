0b85748dd5c88cd5cac4db36e707a415f06be37d
Иван <id@stk-online.ru>
2024-06-11 21:44:43 +0500

    1. Модульные тесты. [#159435 #371215]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,12 +3,12 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        float result = value / 88;
+        float result = value / 70;
         return result;
     }
 
     public static float rubleToDollar(float value) {
-        float result = value / 96; /* формула перевода рублей в доллары. */
+        float result = value / 80;
         return result;
     }
 
